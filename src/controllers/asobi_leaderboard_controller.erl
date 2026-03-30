b3b11884f0e9dcc833bd865b1c6c7bdca8fe774c
Daniel Widgren <daniel.widgren@gmail.com>
2026-03-30 16:51:57 +0200

    fix: return consistent leaderboard entry shape from all endpoints

--- src/controllers/asobi_leaderboard_controller.erl
+++ src/controllers/asobi_leaderboard_controller.erl
@@ -7,30 +7,56 @@ top(#{bindings := #{~"id" := BoardId}, qs := Qs} = _Req) ->
     Params = cow_qs:parse_qs(Qs),
     Limit = binary_to_integer(proplists:get_value(~"limit", Params, ~"100")),
     Entries = asobi_leaderboard_server:top(BoardId, Limit),
-    {json, #{leaderboard_id => BoardId, entries => format_entries(Entries)}}.
+    {json, #{entries => format_entries(BoardId, Entries)}}.
 
 -spec around(cowboy_req:req()) -> {json, map()}.
 around(#{bindings := #{~"id" := BoardId, ~"player_id" := PlayerId}, qs := Qs} = _Req) ->
     Params = cow_qs:parse_qs(Qs),
     Range = binary_to_integer(proplists:get_value(~"range", Params, ~"5")),
     Entries = asobi_leaderboard_server:around(BoardId, PlayerId, Range),
-    {json, #{leaderboard_id => BoardId, entries => format_entries(Entries)}}.
+    {json, #{entries => format_entries(BoardId, Entries)}}.
 
 -spec submit(cowboy_req:req()) -> {json, map()}.
 submit(
     #{bindings := #{~"id" := BoardId}, json := Params, auth_data := #{player_id := PlayerId}} = _Req
 ) ->
     Score = maps:get(~"score", Params),
+    SubScore = maps:get(~"sub_score", Params, 0),
     asobi_leaderboard_server:submit(BoardId, PlayerId, Score),
-    case asobi_leaderboard_server:rank(BoardId, PlayerId) of
-        {ok, Rank} ->
-            {json, #{leaderboard_id => BoardId, rank => Rank, score => Score}};
-        {error, not_found} ->
-            {json, #{leaderboard_id => BoardId, score => Score}}
-    end.
+    Rank =
+        case asobi_leaderboard_server:rank(BoardId, PlayerId) of
+            {ok, R} -> R;
+            {error, not_found} -> null
+        end,
+    {json, #{
+        leaderboard_id => BoardId,
+        player_id => PlayerId,
+        score => Score,
+        sub_score => SubScore,
+        rank => Rank,
+        updated_at => format_timestamp(erlang:system_time(millisecond))
+    }}.
 
 %% --- Internal ---
 
--spec format_entries([{binary(), integer(), pos_integer()}]) -> [map()].
-format_entries(Entries) ->
-    [#{player_id => P, score => S, rank => R} || {P, S, R} <- Entries].
+-spec format_entries(binary(), [{binary(), integer(), pos_integer()}]) -> [map()].
+format_entries(BoardId, Entries) ->
+    [#{
+        leaderboard_id => BoardId,
+        player_id => P,
+        score => S,
+        sub_score => 0,
+        rank => R,
+        updated_at => null
+    } || {P, S, R} <- Entries].
+
+-spec format_timestamp(integer()) -> binary().
+format_timestamp(Ms) ->
+    Seconds = Ms div 1000,
+    {{Y, Mo, D}, {H, Mi, S}} = calendar:system_time_to_universal_time(Seconds, second),
+    iolist_to_binary(
+        io_lib:format(
+            ~"~4..0B-~2..0B-~2..0BT~2..0B:~2..0B:~2..0BZ",
+            [Y, Mo, D, H, Mi, S]
+        )
+    ).
